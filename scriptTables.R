297fd0e3ae83166490e1a35ece5a1fd44799f25a
Rodrigo Lins Rodrigues <rodrigomuribec@gmail.com>
2022-04-06 15:21:32 -0300

    tabela de dupla entrada com testes

---
+++ scriptTables.R
@@ -0,0 +1,18 @@
+#material de referencia https://epirhandbook.com/en/simple-statistical-tests.html
+library(tidyverse) #pacote para manipular dados
+library(gtsummary) #pacote para gerar tabelas
+
+
+#importando a base de dados
+dados <- read.csv("ANÁLISE - Questionários_ Atividades - ganho_naoganho.csv")
+
+
+#tabela simples exemplo
+dados %>% 
+  select( #aqui ta selecionando todas as variáveis na tabela
+    sexo, 
+    estilo_aprendizagem, 
+    resultado_geral) %>% 
+  tbl_summary(
+    by = resultado_geral) %>% #o by informa a varilavel ganho e nao ganho
+  add_p() #adiciona os p-valores dos testes de qui-quadrado
