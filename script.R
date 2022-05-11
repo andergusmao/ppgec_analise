ac5137b0be7593201b59b79f3a519c3e4f30a6a3
Anderson Gusmão <andergusmao@gmail.com>
2022-05-11 16:02:13 -0300

    plotando o gráfico

--- script.R
+++ script.R
@@ -1,17 +1,24 @@
-gitcreds::gitcreds_set()
+#gitcreds::gitcreds_set()
 
 library(tidyverse)
 
 #carregando a base de dados geral
-dados <- read.csv("ANÁLISE - Questionários_ Atividades - ganho_naoganho.csv")
+dados <- read.csv("ANÁLISE - Questionários_ Atividades - index.csv")
+#dados <- read.csv("ANÁLISE - Questionários_ Atividades - ganho_naoganho.csv")
 view(dados)
 
 #plotando o gráfico do ganho e não ganho do pré e pós teste 
 #title='Ganho de Aprendizagem do Pré e Pós Teste' 
-dados %>% ggplot(aes(resultado_geral, ganho_geral, fill=resultado_geral, color=resultado_geral)) + 
+dados %>% ggplot(aes(resultado_geral, nota_geral, fill=resultado_geral, color=resultado_geral)) + 
   geom_boxplot(alpha=0.3) + geom_jitter() +
   labs(x=NULL, y=NULL, fill=NULL, color=NULL)+
   theme_minimal()
+
+
+
+
+
+
 
 #plotando o gráfico do ganho e não ganho do pré e pós teste por sexo
 dados %>% ggplot(aes(resultado_geral, ganho_geral, fill=sexo)) + 
